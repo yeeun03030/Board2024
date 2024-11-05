63079de82c0672df8c3095fad78a39fecf847c3d
yeeun03030 <skdPdms04@gmail.com>
2024-11-05 16:27:34 +0900

    조회화면(read.html)에서 댓글목록을 html 형태로 출력, 모달창 html 작성, 모달창을 통해서 댓글을 등록

--- src/main/java/kr/ac/kopo/board/controller/ReplyController.java
+++ src/main/java/kr/ac/kopo/board/controller/ReplyController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,11 @@ public class ReplyController {
     @GetMapping(value = "/board/{bno}")
     public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno) {
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO) {
+        Long rno = replyService.register(replyDTO);
+        return new ResponseEntity<>(rno, HttpStatus.OK);
     }
 }
