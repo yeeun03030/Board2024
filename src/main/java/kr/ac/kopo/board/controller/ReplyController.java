aa8e4db72678b998ef6fcb3f2f70bb34ecc31d10
yeeun03030 <skdPdms04@gmail.com>
2024-11-12 15:09:44 +0900

    reply done

--- src/main/java/kr/ac/kopo/board/controller/ReplyController.java
+++ src/main/java/kr/ac/kopo/board/controller/ReplyController.java
@@ -27,4 +27,18 @@ public class ReplyController {
         Long rno = replyService.register(replyDTO);
         return new ResponseEntity<>(rno, HttpStatus.OK);
     }
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+        replyService.remove(rno);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO) {
+        replyService.modify(replyDTO);
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
 }
